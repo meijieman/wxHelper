3a5d559434a628698c664e51ff3b953b9e170517
meijieman <jie.mei@lunzn.com>
2019-04-10 19:02:32 +0800

    add

--- app/src/main/java/com/major/wxhelper/MainActivity.java
+++ app/src/main/java/com/major/wxhelper/MainActivity.java
@@ -2,9 +2,18 @@ package com.major.wxhelper;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.accessibility.AccessibilityManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
     public static final String WECAHT_PACKAGENAME = "com.tencent.mm";
 
     //微信首页
@@ -35,5 +44,113 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
 
         });
+
+        init();
+    }
+
+    private TextView start, sendStatus;
+    private EditText sendName, sendContent;
+    private AccessibilityManager accessibilityManager;
+    private String name, content;
+
+
+    private void init() {
+        start = findViewById(R.id.testWechat);
+        sendName = findViewById(R.id.sendName);
+        sendContent = findViewById(R.id.sendContent);
+        sendStatus = findViewById(R.id.sendStatus);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAndStartService();
+            }
+        });
+    }
+
+    private int goWecaht() {
+        try {
+            setValue(name, content);
+            MyAccessibilityService.hasSend = false;
+            Intent intent = new Intent();
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setClassName(WeChatTextWrapper.WECAHT_PACKAGENAME, WeChatTextWrapper.WechatClass.WECHAT_CLASS_LAUNCHUI);
+            startActivity(intent);
+
+            while (true) {
+                if (MyAccessibilityService.hasSend) {
+                    return MyAccessibilityService.SEND_STATUS;
+                } else {
+                    try {
+                        Thread.sleep(500);
+                    } catch (Exception e) {
+                        openService();
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return MyAccessibilityService.SEND_STATUS;
+        }
+    }
+
+
+    private void openService() {
+        try {
+            //打开系统设置中辅助功能
+            Intent intent = new Intent(android.provider.Settings.ACTION_ACCESSIBILITY_SETTINGS);
+            startActivity(intent);
+            Toast.makeText(MainActivity.this, "找到微信自动发送消息，然后开启服务即可", Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void checkAndStartService() {
+        accessibilityManager = (AccessibilityManager) getSystemService(ACCESSIBILITY_SERVICE);
+
+        name = sendName.getText().toString();
+        content = sendContent.getText().toString();
+
+        if (TextUtils.isEmpty(name)) {
+            Toast.makeText(MainActivity.this, "联系人不能为空", Toast.LENGTH_SHORT);
+        }
+        if (TextUtils.isEmpty(content)) {
+            Toast.makeText(MainActivity.this, "内容不能为空", Toast.LENGTH_SHORT);
+        }
+
+        if (!accessibilityManager.isEnabled()) {
+            openService();
+        } else {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    statusHandler.sendEmptyMessage(goWecaht());
+                }
+            }).start();
+        }
+    }
+
+    Handler statusHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            setSendStatusText(msg.what);
+        }
+    };
+
+    private void setSendStatusText(int status) {
+        if (status == MyAccessibilityService.SEND_SUCCESS) {
+            sendStatus.setText("微信发送成功");
+        } else {
+            sendStatus.setText("微信发送失败");
+        }
+    }
+
+    public void setValue(String name, String content) {
+        WechatUtils.NAME = name;
+        WechatUtils.CONTENT = content;
+        MyAccessibilityService.hasSend = false;
     }
 }
--- app/src/main/java/com/major/wxhelper/MyAccessibilityService.java
+++ app/src/main/java/com/major/wxhelper/MyAccessibilityService.java
@@ -1,11 +1,16 @@
 package com.major.wxhelper;
 
 import android.accessibilityservice.AccessibilityService;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 
 import com.major.base.log.LogUtil;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -24,7 +29,7 @@ public class MyAccessibilityService extends AccessibilityService {
         LogUtil.init(getPackageName(), "tag_wx", true, false);
     }
 
-    @Override
+ /*   @Override
     protected void onServiceConnected() {
 //        AccessibilityServiceInfo info = getServiceInfo();
 //        //这里可以设置多个包名，监听多个应用
@@ -72,10 +77,195 @@ public class MyAccessibilityService extends AccessibilityService {
 
                 break;
         }
-    }
+    }*/
 
     @Override
     public void onInterrupt() {
 
+    }
+
+    private static final String TAG = "AutoSendMsgService";
+    private List<String> allNameList = new ArrayList<>();
+    private int mRepeatCount;
+
+    public static boolean hasSend;
+    public static final int SEND_FAIL = 0;
+    public static final int SEND_SUCCESS = 1;
+    public static int SEND_STATUS;
+
+    /**
+     * 必须重写的方法，响应各种事件。
+     *
+     * @param event
+     */
+    @Override
+    public void onAccessibilityEvent(final AccessibilityEvent event) {
+        int eventType = event.getEventType();
+        switch (eventType) {
+            case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED: {
+
+                String currentActivity = event.getClassName().toString();
+
+                if (hasSend) {
+                    return;
+                }
+
+                if (currentActivity.equals(WeChatTextWrapper.WechatClass.WECHAT_CLASS_LAUNCHUI)) {
+                    handleFlow_LaunchUI();
+                } else if (currentActivity.equals(WeChatTextWrapper.WechatClass.WECHAT_CLASS_CONTACTINFOUI)) {
+                    handleFlow_ContactInfoUI();
+                } else if (currentActivity.equals(WeChatTextWrapper.WechatClass.WECHAT_CLASS_CHATUI)) {
+                    handleFlow_ChatUI();
+                }
+            }
+            break;
+        }
+    }
+
+    private void handleFlow_ChatUI() {
+
+        //如果微信已经处于聊天界面，需要判断当前联系人是不是需要发送的联系人
+        String curUserName = WechatUtils.findTextById(this, WeChatTextWrapper.WechatId.WECHATID_CHATUI_USERNAME_ID);
+        if (!TextUtils.isEmpty(curUserName) && curUserName.equals(WechatUtils.NAME)) {
+            if (WechatUtils.findViewByIdAndPasteContent(this, WeChatTextWrapper.WechatId.WECHATID_CHATUI_EDITTEXT_ID, WechatUtils.CONTENT)) {
+                sendContent();
+            } else {
+                //当前页面可能处于发送语音状态，需要切换成发送文本状态
+                WechatUtils.findViewIdAndClick(this, WeChatTextWrapper.WechatId.WECHATID_CHATUI_SWITCH_ID);
+
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                if (WechatUtils.findViewByIdAndPasteContent(this, WeChatTextWrapper.WechatId.WECHATID_CHATUI_EDITTEXT_ID, WechatUtils.CONTENT)) {
+                    sendContent();
+                }
+            }
+        } else {
+            //回到主界面
+            WechatUtils.findViewIdAndClick(this, WeChatTextWrapper.WechatId.WECHATID_CHATUI_BACK_ID);
+        }
+    }
+
+    private void handleFlow_ContactInfoUI() {
+        WechatUtils.findTextAndClick(this, "发消息");
+    }
+
+    private void handleFlow_LaunchUI() {
+
+        try {
+            //点击通讯录，跳转到通讯录页面
+            WechatUtils.findTextAndClick(this, "通讯录");
+
+            Thread.sleep(50);
+
+            //再次点击通讯录，确保通讯录列表移动到了顶部
+            WechatUtils.findTextAndClick(this, "通讯录");
+
+            Thread.sleep(200);
+
+            //遍历通讯录联系人列表，查找联系人
+            AccessibilityNodeInfo itemInfo = TraversalAndFindContacts();
+            if (itemInfo != null) {
+                WechatUtils.performClick(itemInfo);
+            } else {
+                SEND_STATUS = SEND_FAIL;
+                resetAndReturnApp();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 从头至尾遍历寻找联系人
+     *
+     * @return
+     */
+    private AccessibilityNodeInfo TraversalAndFindContacts() {
+
+        if (allNameList != null) allNameList.clear();
+
+        AccessibilityNodeInfo rootNode = getRootInActiveWindow();
+        List<AccessibilityNodeInfo> listview = rootNode.findAccessibilityNodeInfosByViewId(WeChatTextWrapper.WechatId.WECHATID_CONTACTUI_LISTVIEW_ID);
+
+        //是否滚动到了底部
+        boolean scrollToBottom = false;
+        if (listview != null && !listview.isEmpty()) {
+            while (true) {
+                //获取当前屏幕上的联系人信息
+                List<AccessibilityNodeInfo> nameList = rootNode.findAccessibilityNodeInfosByViewId(WeChatTextWrapper.WechatId.WECHATID_CONTACTUI_NAME_ID);
+                List<AccessibilityNodeInfo> itemList = rootNode.findAccessibilityNodeInfosByViewId(WeChatTextWrapper.WechatId.WECHATID_CONTACTUI_ITEM_ID);
+
+                if (nameList != null && !nameList.isEmpty()) {
+                    for (int i = 0; i < nameList.size(); i++) {
+                        if (i == 0) {
+                            //必须在一个循环内，防止翻页的时候名字发生重复
+                            mRepeatCount = 0;
+                        }
+                        AccessibilityNodeInfo itemInfo = itemList.get(i);
+                        AccessibilityNodeInfo nodeInfo = nameList.get(i);
+                        String nickname = nodeInfo.getText().toString();
+                        Log.d(TAG, "nickname = " + nickname);
+                        if (nickname.equals(WechatUtils.NAME)) {
+                            return itemInfo;
+                        }
+                        if (!allNameList.contains(nickname)) {
+                            allNameList.add(nickname);
+                        } else if (allNameList.contains(nickname)) {
+                            Log.d(TAG, "mRepeatCount = " + mRepeatCount);
+                            if (mRepeatCount == 3) {
+                                //表示已经滑动到顶部了
+                                if (scrollToBottom) {
+                                    Log.d(TAG, "没有找到联系人");
+                                    //此次发消息操作已经完成
+                                    hasSend = true;
+                                    return null;
+                                }
+                                scrollToBottom = true;
+                            }
+                            mRepeatCount++;
+                        }
+                    }
+                }
+
+                if (!scrollToBottom) {
+                    //向下滚动
+                    listview.get(0).performAction(AccessibilityNodeInfo.ACTION_SCROLL_FORWARD);
+                } else {
+                    return null;
+                }
+
+                //必须等待，因为需要等待滚动操作完成
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+
+    private void sendContent() {
+        WechatUtils.findTextAndClick(this, "发送");
+        SEND_STATUS = SEND_SUCCESS;
+        resetAndReturnApp();
+    }
+
+    private void resetAndReturnApp() {
+        hasSend = true;
+        ActivityManager activtyManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningTaskInfo> runningTaskInfos = activtyManager.getRunningTasks(3);
+        for (ActivityManager.RunningTaskInfo runningTaskInfo : runningTaskInfos) {
+            if (this.getPackageName().equals(runningTaskInfo.topActivity.getPackageName())) {
+                activtyManager.moveTaskToFront(runningTaskInfo.id, ActivityManager.MOVE_TASK_WITH_HOME);
+                return;
+            }
+        }
     }
 }
